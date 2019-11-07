acdc7a2cc44bad50abb4b5bb7e8388305260060f
Angus Morrison <contact@angus-morrison.com>
2019-11-07 17:14:30 +0000

    Drafted User and Follow relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :avatar
-en
+
+  has_many :follows_as_followed, foreign_key: :followed_id, class_name: :Follow, dependent: :destroy
+  has_many :follows_as_follower, foreign_key: :follower_id, class_name: :Follow, dependent: :destroy
+end
