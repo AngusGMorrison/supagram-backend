395cca28a8f7a8d86bea5378483517b953126873
Angus Morrison <contact@angus-morrison.com>
2019-11-07 17:23:49 +0000

    Drafted User/Post relationships

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+
+  has_one_attached :photo
+
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  
   has_one_attached :avatar
 
   has_many :follows_as_followed, foreign_key: :followed_id, class_name: :Follow, dependent: :destroy
   has_many :follows_as_follower, foreign_key: :follower_id, class_name: :Follow, dependent: :destroy
+
+  has_many :posts, dependent: :destroy
+
 end
