fdfb51bc71807434fe286b30a860e0360f7ec361
Angus Morrison <contact@angus-morrison.com>
2019-11-08 16:43:45 +0000

    Created UserFactory

---
+++ spec/support/factories/user_factory.rb
@@ -0,0 +1,17 @@
+class UserFactory
+
+  NAME: Faker::Name.first_name,
+  USERNAME: Faker::Internet.username(specifier: 4..30),
+  EMAIL: Faker::Internet.email,
+  PASSWORD: "password1^"
+
+  def self.create(name: NAME, username: USERNAME, email: EMAIL, password: PASSWORD)
+    User.create(
+      name: name,
+      username: username,
+      email: email,
+      password: password
+    )
+  end
+
+end
