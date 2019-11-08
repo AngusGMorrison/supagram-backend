567c2aa2702c863c87c3b9fcc53b351d27cbd314
Angus Morrison <contact@angus-morrison.com>
2019-11-08 16:46:48 +0000

    Fixed UserFactory constant assignment

--- spec/support/factories/user_factory.rb
+++ spec/support/factories/user_factory.rb
@@ -1,9 +1,9 @@
 class UserFactory
 
-  NAME: Faker::Name.first_name,
-  USERNAME: Faker::Internet.username(specifier: 4..30),
-  EMAIL: Faker::Internet.email,
-  PASSWORD: "password1^"
+  NAME = Faker::Name.first_name,
+  USERNAME = Faker::Internet.username(specifier: 4..30),
+  EMAIL = Faker::Internet.email,
+  PASSWORD = "password1^"
 
   def self.create(name: NAME, username: USERNAME, email: EMAIL, password: PASSWORD)
     User.create(
