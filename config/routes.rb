2d2fcc7942b5f54e07137fce8d3f580df35ee0a1
Angus Morrison <contact@angus-morrison.com>
2019-11-08 14:41:30 +0000

    Sign up route working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/sign-in", to: "users#sign_in", as: "sign_in"
-  post "/signup", to: "users#signup", as: "signup"
+  post "/sign-up", to: "users#sign_up", as: "sign_up"
 end
