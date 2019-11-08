1b043872d35fc3b814c2aa25adffb16adcb14fac
Angus Morrison <contact@angus-morrison.com>
2019-11-08 16:48:18 +0000

    Removed commas breaking UserFactory constants

--- spec/support/factories/user_factory.rb
+++ spec/support/factories/user_factory.rb
@@ -1,8 +1,8 @@
 class UserFactory
 
-  NAME = Faker::Name.first_name,
-  USERNAME = Faker::Internet.username(specifier: 4..30),
-  EMAIL = Faker::Internet.email,
+  NAME = Faker::Name.first_name
+  USERNAME = Faker::Internet.username(specifier: 4..30)
+  EMAIL = Faker::Internet.email
   PASSWORD = "password1^"
 
   def self.create(name: NAME, username: USERNAME, email: EMAIL, password: PASSWORD)
