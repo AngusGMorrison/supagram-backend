44cca7633f1c214ec1bde09689636095df419ab2
Angus Morrison <contact@angus-morrison.com>
2019-11-09 15:02:20 +0000

    Added incorrect email and password constants

--- spec/support/test_constants.rb
+++ spec/support/test_constants.rb
@@ -2,5 +2,8 @@ module TestConstants
 
   EMAIL = "test@test.com"
   PASSWORD = "password1^"
+  SECRET_KEY = ENV["SUPAGRAM_JWT_KEY"]
+  INCORRECT_EMAIL = "fake@fake.com"
+  INCORRECT_PASSWORD = "fakepassword1^"
   
 end
