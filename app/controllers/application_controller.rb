3b5b5fa5ff691914c79b09341a1468d948e1a6cc
Angus Morrison <contact@angus-morrison.com>
2019-11-10 11:32:59 +0000

    Authorization working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,19 @@ class ApplicationController < ActionController::API
     render json: { errors: "Parameter missing" }, status: 400
   end
 
+  def get_current_user
+    user_id = decode_token()["user_id"]
+    User.find_by(id: user_id)
+  end
+
   def issue_token(data)
     JWT.encode(data, get_secret())
   end
 
-  # def decode_token
-  #   token = request.headers["token"]
-  # end
+  def decode_token
+    token = request.headers["Authorization"]
+    JWT.decode(token, get_secret()).first
+  end
 
   def get_secret
     ENV["SUPAGRAM_JWT_KEY"]
