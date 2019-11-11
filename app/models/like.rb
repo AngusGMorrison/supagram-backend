0b44675f8e0ca63f414721045b31a0f11e3772c2
Angus Morrison <contact@angus-morrison.com>
2019-11-11 17:30:12 +0000

    Added uniqueness validation for likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,14 @@
 class Like < ApplicationRecord
+  include ValidationErrorMessages
 
   belongs_to :post
   belongs_to :user
+
+  validates :user_id, {
+    uniqueness: { 
+      scope: :post_id,
+      message: ALREADY_LIKED
+    }
+  }
   
 end
