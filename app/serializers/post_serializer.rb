e76f4549c80173e02c88d8a450cf139dbcee6c64
Angus Morrison <contact@angus-morrison.com>
2019-11-12 09:45:07 +0000

    Author details moved into sub-hash with followed_by_user attribute

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -10,14 +10,17 @@ class PostSerializer
     {
       post: {
         id: @post.id,
-        creator_id: @post.user.id,
-        username: @post.user.username,
         image_url: @post.get_image_url,
         caption: @post.caption,
         most_recent_likes: @post.get_most_recent_likes(),
         like_count: @post.likes.length,
         created_at: @post.created_at,
         liked_by_user: @post.liked_by?(@user)
+        author: {
+          author_id: @post.user.id,
+          username: @post.user.username,
+          followed_by_user: @post.user.followed_by?(@user)
+        }
       },
       user: {
         post_count: @user.get_post_count()
