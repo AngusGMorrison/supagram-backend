5123e1c0d38bdb3e0d339b2cd93d812432bc257f
Angus Morrison <contact@angus-morrison.com>
2019-11-12 19:47:06 +0000

    Added avatar to serialized json

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer
+  include Rails.application.routes.url_helpers
 
   def initialize(user, token)
     @user = user
@@ -10,11 +11,18 @@ class UserSerializer
       token: @token,
       user: {
         username: @user.username,
+        avatar: get_avatar_url(),
         post_count: @user.get_post_count(),
         follower_count: @user.get_follower_count(),
         followed_count: @user.get_followed_count()
       }
     }.to_json()
+  end
+
+  private def get_avatar_url()
+    url_for(@user.avatar)
+  end
+
   end
 
 end
