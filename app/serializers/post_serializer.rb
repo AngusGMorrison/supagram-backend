213f6c1b210cf53f4a3f0ff718c6fa9bfbdc01c1
Angus Morrison <contact@angus-morrison.com>
2019-11-12 11:05:27 +0000

    Fixed missing comma

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -15,7 +15,7 @@ class PostSerializer
         most_recent_likes: @post.get_most_recent_likes(),
         like_count: @post.likes.length,
         created_at: @post.created_at,
-        liked_by_current_user: @post.liked_by?(@user)
+        liked_by_current_user: @post.liked_by?(@user),
         author: {
           author_id: @post.user.id,
           username: @post.user.username,
