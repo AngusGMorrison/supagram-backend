da305346edb9e298140022b451217426f01d3458
Angus Morrison <contact@angus-morrison.com>
2019-11-12 12:49:02 +0000

    FollowSerializer refactored to use single method for both follow and unfollow

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,32 +1,36 @@
 class FollowsController < ApplicationController
 
   def create
-    user = get_current_user()
-    params[:follow][:follower_id] = user.id
+    @user = get_current_user()
+    params[:follow][:follower_id] = @user.id
     @follow = Follow.create(follow_params())
-    respond_to_follow()
+    respond_to_create_follow()
   end
 
   private def follow_params
     params.require(:follow).permit(:follower_id, :followed_id)
   end
 
-  private def respond_to_follow
+  private def respond_to_create_follow
     if @follow.valid?()
-      follow_serializer = FollowSerializer.new(@follow)
-      render json: follow_serializer.serialize_follow(), status: 200
+      follow_serializer = FollowSerializer.new(@follow, @user)
+      render json: follow_serializer.serialize(), status: 200
     else
       render_errors()
     end
   end
 
   def destroy
-    user = get_current_user()
-    @follow = Follow.find_by(follower_id: user.id, followed_id: params[:follow][:followed_id])
+    @user = get_current_user()
+    @follow = Follow.find_by(follower_id: @user.id, followed_id: params[:follow][:followed_id])
+    respond_to_destroy_follow()
+  end
+
+  private def respond_to_destroy_follow
     if @follow.valid?()
       @follow.destroy()
-      follow_serializer = FollowSerializer.new(@follow)
-      render json: follow_serializer.serialize_unfollow(), status: 200
+      follow_serializer = FollowSerializer.new(@follow, @user)
+      render json: follow_serializer.serialize(), status: 200
     else
       render_errors()
     end
--- app/serializers/follow_serializer.rb
+++ app/serializers/follow_serializer.rb
@@ -1,25 +1,16 @@
 class FollowSerializer
 
-  def initialize(follow)
-    @follow = follow
+  def initialize(follow, user)
+    @followed = follow.followed
+    @user = user
   end
 
-  def serialize_follow
+  def serialize
     {
       user: {
-        id: @follow.followed_id,
-        username: @follow.followed.username,
-        followed_by_current_user: true
-      }
-    }.to_json()
-  end
-
-  def serialize_unfollow
-    {
-      user: {
-        id: @follow.followed_id,
-        username: @follow.followed.username,
-        followed_by_current_user: false
+        id: @followed.id,
+        username: @followed.username,
+        followed_by_current_user: @followed.followed_by?(@user)
       }
     }.to_json()
   end
