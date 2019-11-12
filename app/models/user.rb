68eead0a3418df095dd73f2281a2ecb8da164566
Angus Morrison <contact@angus-morrison.com>
2019-11-12 11:40:33 +0000

    Polymorphic follow relationship working correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   include ValidationErrorMessages
 
   has_one_attached :avatar
-  has_many :followers, foreign_key: :followed_id, class_name: :Follow, dependent: :destroy
-  has_many :followed, foreign_key: :follower_id, class_name: :Follow, dependent: :destroy
+  has_many :follows_as_follower, foreign_key: :follower_id, class_name: :Follow, dependent: :destroy
+  has_many :follows_as_followed, foreign_key: :followed_id, class_name: :Follow, dependent: :destroy
+  has_many :followed, through: :follows_as_follower, class_name: :User
+  has_many :followers, through: :follows_as_followed, class_name: :User
+
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
 
