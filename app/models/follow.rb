839bebb7531f452bfd1e670c8eadbbd40f7ff7c0
Angus Morrison <contact@angus-morrison.com>
2019-11-12 10:41:24 +0000

    Added duplicate prevention

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,5 +2,9 @@ class Follow < ApplicationRecord
 
   belongs_to :followed, class_name: :User
   belongs_to :follower, class_name: :User
+
+  validates :follower_id, {
+    uniqueness: { scope: followed_id }
+  }
   
 end
