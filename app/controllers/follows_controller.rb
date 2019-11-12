f9f618a11e824216caca7a06439adcf2d9f48e3d
Angus Morrison <contact@angus-morrison.com>
2019-11-12 10:39:45 +0000

    Skeleton for follow creation

---
+++ app/controllers/follows_controller.rb
@@ -0,0 +1,23 @@
+class FollowController < ApplicationController
+
+  def create
+    user = get_current_user()
+    params[:follow][:follower_id] = user.id
+    @follow = Follow.create(follow_params())
+    respond_to_follow()
+  end
+
+  private def follow_params
+    params.require(:follow).permit(:follower_id, :followed_id)
+  end
+
+  private def respond_to_follow
+    if @follow.valid?()
+      follow_serializer = FollowSerializer.new(@follow)
+      render json: follow_serializer.serialize(), status: 200
+    else
+      render json: { errors: @follow.errors }, status: 400
+    end
+  end
+
+end
