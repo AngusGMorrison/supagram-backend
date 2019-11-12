271bb9eaa899976689633e48fe38c5e3934f5e1e
Angus Morrison <contact@angus-morrison.com>
2019-11-12 11:54:30 +0000

    Validates on uniqueness and follow self

--- app/models/concerns/validation_error_messages.rb
+++ app/models/concerns/validation_error_messages.rb
@@ -3,6 +3,7 @@ module ValidationErrorMessages
   PASSWORD_MESSAGE = "must have at least 8 characters, 1 number and 1 special character"
   USERNAME_MESSAGE = "must start and end with a letter, number or _, and contain no special characters other than . and _"
   ALREADY_LIKED = "has already liked this post"
-  CANT_FOLLOW_SELF = "can't follow themselves"
+  CANNOT_FOLLOW_SELF = "can't follow themselves"
+  ALREADY_FOLLOWED = "has already followed this user"
 
 end
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -4,11 +4,19 @@ class Follow < ApplicationRecord
   belongs_to :followed, class_name: :User
   belongs_to :follower, class_name: :User
 
+  validate :cannot_follow_self
+
   validates :follower, {
     uniqueness: {
       scope: :followed,
-      message: CANT_FOLLOW_SELF
+      message: ALREADY_FOLLOWED
     }
   }
+
+  def cannot_follow_self
+    if follower == followed
+      errors.add(:follower, CANNOT_FOLLOW_SELF)
+    end
+  end
   
 end
