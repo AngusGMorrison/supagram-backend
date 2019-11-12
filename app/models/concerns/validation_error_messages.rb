e78530a70f39a5bbddf65a0bcf362da30731788f
Angus Morrison <contact@angus-morrison.com>
2019-11-12 11:11:19 +0000

    Added custom error message for self-follows

--- app/models/concerns/validation_error_messages.rb
+++ app/models/concerns/validation_error_messages.rb
@@ -3,5 +3,6 @@ module ValidationErrorMessages
   PASSWORD_MESSAGE = "must have at least 8 characters, 1 number and 1 special character"
   USERNAME_MESSAGE = "must start and end with a letter, number or _, and contain no special characters other than . and _"
   ALREADY_LIKED = "has already liked this post"
+  CANT_FOLLOW_SELF = "can't follow themselves"
 
 end
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,10 +1,14 @@
 class Follow < ApplicationRecord
+  include ValidationErrorMessages
 
   belongs_to :followed, class_name: :User
   belongs_to :follower, class_name: :User
 
   validates :follower, {
-    uniqueness: { scope: :followed }
+    uniqueness: {
+      scope: :followed,
+      message: CANT_FOLLOW_SELF
+    }
   }
   
 end
