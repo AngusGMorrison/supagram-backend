e24b774aec1fa0dabc17b92bc823f75583221ec0
Angus Morrison <contact@angus-morrison.com>
2019-11-12 11:03:41 +0000

    Corrected uniqueness scope format

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,8 +3,8 @@ class Follow < ApplicationRecord
   belongs_to :followed, class_name: :User
   belongs_to :follower, class_name: :User
 
-  validates :follower_id, {
-    uniqueness: { scope: followed_id }
+  validates :follower, {
+    uniqueness: { scope: :followed }
   }
   
 end
