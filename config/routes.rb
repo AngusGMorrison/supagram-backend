a4e782a8363c0f7abfbb83237a159940f35c4fe7
Angus Morrison <contact@angus-morrison.com>
2019-11-12 09:09:24 +0000

    Added create follow route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post "/sign-in", to: "users#sign_in", as: "sign_in"
-  post "/sign-up", to: "users#sign_up", as: "sign_up"
+  post "/sign-in", to: "users#sign_in"
+  post "/sign-up", to: "users#sign_up"
 
-  post "/posts", to: "posts#create", as: "create_post"
-  post "/posts/:post_id", to: "posts#like", as: "like_post"
-  delete "/posts/:post_id", to: "posts#unlike", as: "unlike_post"
+  post "/posts", to: "posts#create"
+  post "/posts/:post_id", to: "posts#like"
+  delete "/posts/:post_id", to: "posts#unlike"
+
+  post "/follow", to: "follows#create"
 
 end
