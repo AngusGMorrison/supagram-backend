a198d69bbf9665c66703385383cddb66a0e3dd0a
Angus Morrison <contact@angus-morrison.com>
2019-11-13 15:14:58 +0000

    Added users#show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/sign-in", to: "users#sign_in"
   post "/sign-up", to: "users#sign_up"
-  put "/users/:id/avatar", to: "users#change_avatar"
+  get "/users/:username", to: "users#show"
+  put "/users/:username/avatar", to: "users#change_avatar"
 
   get "/posts", to: "posts#show_feed"
   post "/posts", to: "posts#create"
